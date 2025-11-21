b9f57d673afa649a824995155c693349dc3526b3
essadikine ayman <r140137771@taalim.ma>
2025-11-21 19:08:04 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './assets/scss/style.scss'
+import '../node_modules/animate.css/animate.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
