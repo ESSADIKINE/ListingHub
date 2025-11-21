69db85b0f344e59915fb898ecd58ca7f48e4932a
Anass Essadikine <140465843+ESSADIKINE@users.noreply.github.com>
2025-11-21 20:11:43 +0100

    Align ESLint with Next.js requirements

---
+++ pages/_app.js
@@ -0,0 +1,43 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'animate.css/animate.css'
+import '../styles/style.scss'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    let bootstrapModule
+
+    import('bootstrap/dist/js/bootstrap.bundle.min.js').then((module) => {
+      bootstrapModule = module.default
+      const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+      tooltipTriggerList.forEach((tooltipTriggerEl) => {
+        if (bootstrapModule?.Tooltip) {
+          new bootstrapModule.Tooltip(tooltipTriggerEl)
+        }
+      })
+    })
+
+    return () => {
+      if (bootstrapModule?.Tooltip) {
+        document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((tooltipTriggerEl) => {
+          const existing = bootstrapModule.Tooltip.getInstance(tooltipTriggerEl)
+          existing?.dispose()
+        })
+      }
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>ListingHub - React Js Business Directory & Listing Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
